3872f77952f9c52bf4370fd832cd635e8aa1b9d5
BakryDiarra <bakrydiarra@gmail.com>
2023-03-11 14:20:35 +0000

    Add: comment in code. Correct: typing error

--- contact/models.py
+++ contact/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 class ContactForm(models.Model):
 
+    """
+    Using a tuple to propose a multiple choice
+    first element of each tuple is the value
+    that will be stored in the database
+    second element is the human-readable labe
+    """
+
     REASON_CHOICES = (
         ("1", "Registration issue"),
         ("2", "Login issue"),
@@ -22,8 +29,16 @@ class ContactForm(models.Model):
     written_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name = 'Massage'
+        """
+         to specify human-readable names for the model
+         used for instance in Django admin site
+        """
+        verbose_name = 'Message'
         verbose_name_plural = 'Messages'
 
     def __str__(self):
+        """
+         to return contact name and date as a string
+         used for instance in Django admin site
+        """
         return f'Message from {self.name} on {self.written_on}'
