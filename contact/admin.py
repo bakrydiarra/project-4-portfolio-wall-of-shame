b1465fca8089d487177e40dae8c5d93d483cc27f
BakryDiarra <bakrydiarra@gmail.com>
2023-03-12 09:26:26 +0000

    Add: Admin class ContactForm

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(ContactForm)
+class ContactFormAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'email', 'message', 'reason', 'written_on')
+    summernote_fields = ('message')
+    list_filter = ('name', 'reason', 'written_on')
