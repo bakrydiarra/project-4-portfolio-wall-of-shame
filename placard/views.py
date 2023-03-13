478fb7de9fcee32785ca8b54209b6738cac81364
BakryDiarra <bakrydiarra@gmail.com>
2023-03-13 10:57:32 +0000

    Add: view PersonaList

--- placard/views.py
+++ placard/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import *
 
-# Create your views here.
+
+class PersonaList(generic.ListView):
+    model = Persona
+    queryset = Persona.order_by('created_on')
+    template_name = 'persona_details.html'
+    paginate_by = 6
